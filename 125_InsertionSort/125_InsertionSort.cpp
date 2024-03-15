c28d35eb83c5b1f711bf7cad2c4475e06c8533a8
jihaduttolibinn <jihaduttolibin02@gmail.com>
2024-03-15 10:56:20 +0700

    memasukkan void

--- 125_InsertionSort/125_InsertionSort.cpp
+++ 125_InsertionSort/125_InsertionSort.cpp
@@ -1,12 +1,65 @@
 
 #include <iostream>
 using namespace std;
+int arr[11];
+int n;
+
+void input() {
+	while (true) {
+		cout << " Masukan banyaknya elemen pada array : ";
+		cin >> n;
+		if (n <= 20)
+			break;
+		else {
+			cout << "\nArray dapat mempunyai maksimal 20.\n";
+		}
+	}
+	cout << endl;
+	cout << "====================" << endl;
+	cout << "Masukan Elemen Array" << endl;
+	cout << "====================" << endl;
+
+	for (int i = 0; i < n; i++) {
+		cout << "Data ke-" << (i + 1) << ": ";
+		cin >> arr[i];
+	}
+}
+
+
 void insertionsort() {
     int i, n, temp, j;
-    int arr[11];
+    
+
+    for (i = 1; i <= n - 1; i++)
+    {
+        temp = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > temp)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = temp;
+         
+    }
 
 
 }
+
+void display() {
+	cout << endl;
+	cout << "=================================" << endl;
+	cout << "Element attay yang telah tersusun" << endl;
+	cout << "=================================" << endl;
+	for (int j = 0; j < n; j++) {
+		cout << arr[j] << endl;
+
+	}
+	cout << "jumlah pass = " << n - 1 << endl;
+	cout << endl;
+}
+
+
 int main()
 {
     std::cout << "Hello World!\n";
